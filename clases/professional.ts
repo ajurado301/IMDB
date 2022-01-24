8a6d049b2527749a8f1354f8c2c1fa92c0783444
ajurado301 <ajurado301@gmail.com>
2022-01-24 09:01:38 +0100

    Commit sección avanzada - Parte 2

--- clases/professional.ts
+++ clases/professional.ts
@@ -2,18 +2,18 @@
 export class Professional {
 
     // Atributos privados
-    name: string;
-    age: number;
-    genre: string;
-    weight: number;
-    height: number;
-    hairColor: string;
-    eyeColor: string;
-    race: string;
-    isRetired: boolean;
-    nationality: string;
-    oscarsNumber: number;
-    profession:string;
+    public name: string;
+    public age: number;
+    public genre: string;
+    public weight: number;
+    public height: number;
+    public hairColor: string;
+    public eyeColor: string;
+    public race: string;
+    public isRetired: boolean;
+    public nationality: string;
+    public oscarsNumber: number;
+    public profession:string;
     
     // Constructor
     constructor(name        : string,
@@ -42,44 +42,6 @@ export class Professional {
         this.oscarsNumber = oscarsNumber;
         this.profession   = profession;
     }
-
-    // getters y setters
-    // name
-    public getName(): string { return this.name };
-    public setName(name: string): void { this.name = name };
-    // age
-    public getAge(): number { return this.age };
-    public setAge(age: number): void { this.age = age };
-    // genre
-    public getGenre(): string { return this.genre };
-    public setGenre(genre: string): void { this.genre = genre };
-    // weight
-    public getWeight(): number { return this.weight };
-    public setWeight(weight: number): void { this.weight = weight };
-    // height
-    public getHeight(): number { return this.height };
-    public setHeight(height: number): void { this.height = height };
-    // hairColor
-    public getHairColor(): string { return this.hairColor };
-    public setHairColor(hairColor: string): void { this.hairColor = hairColor };
-    // eyeColor
-    public getEyeColor(): string { return this.eyeColor };
-    public setEyeColor(eyeColor: string): void { this.eyeColor = eyeColor };
-    // race
-    public getRace(): string { return this.race };
-    public setRace(race: string): void { this.race = race };
-    // isRetired
-    public getIsRetired(): boolean { return this.isRetired };
-    public setIsRetired(isRetired: boolean): void { this.isRetired = isRetired };
-    // nationality
-    public getNationality(): string { return this.nationality };
-    public setNationality(nationality: string): void { this.nationality = nationality };
-    // oscarsNumber
-    public getOscarsNumber(): number { return this.oscarsNumber };
-    public setOscarsNumber(oscarsNumber: number): void { this.oscarsNumber = oscarsNumber };
-    // profession
-    public getProfession(): string { return this.profession };
-    public setProfession(profession: string): void { this.profession = profession };
 
     // Métodos públicos
     public printProfessional(): void {
--- main/professionalMain.ts
+++ main/professionalMain.ts
@@ -9,49 +9,6 @@ let profesional4: Professional = new Professional("James Cameron", 67, "Masculin
 let profesional5: Professional = new Professional("Quentin Tarantino", 58, "Masculino", 92, 185, "Marrón oscuro", "Café", "Caucásica", false, "Estadounidense", 2, "Guionista");
 let profesional6: Professional = new Professional("Joel Silver", 69, "Masculino", 84, 175, "Negro", "Negro", "Caucásica", false, "Estadounidense", 0, "Productor");
 
-// Probar getters y setters con profesional1
-// getters
-console.log("Valores actuales de profesional1 mostrados con getters:");
-console.log("Nombre       :", profesional1.getName());
-console.log("Edad         :", profesional1.getAge());
-console.log("Genero       :", profesional1.getGenre());
-console.log("Peso         :", profesional1.getWeight());
-console.log("Altura       :", profesional1.getHeight());
-console.log("Color de pelo:", profesional1.getHairColor());
-console.log("Color de ojos:", profesional1.getEyeColor());
-console.log("Raza         :", profesional1.getRace());
-console.log("Retirado?    :", profesional1.getIsRetired());
-console.log("Nacionalidad :", profesional1.getNationality());
-console.log("Nº de Oscars :", profesional1.getOscarsNumber());
-console.log("Profesión    :", profesional1.getProfession());
-// setters
-console.log("Valores de profesional1 cambiados con setters y mostrados con getters:");
-profesional1.setName("Charlize Theron");
-profesional1.setAge(46);
-profesional1.setGenre("Femenino")
-profesional1.setWeight(58);
-profesional1.setHeight(177);
-profesional1.setHairColor("Rubio");
-profesional1.setEyeColor("Azul");
-profesional1.setRace("Caucásica");
-profesional1.setIsRetired(false);
-profesional1.setNationality("Sudafricana");
-profesional1.setOscarsNumber(1);
-profesional1.setProfession("Actriz");
-// Volver a mostrar
-console.log("Nombre       :", profesional1.getName());
-console.log("Edad         :", profesional1.getAge());
-console.log("Genero       :", profesional1.getGenre());
-console.log("Peso         :", profesional1.getWeight());
-console.log("Altura       :", profesional1.getHeight());
-console.log("Color de pelo:", profesional1.getHairColor());
-console.log("Color de ojos:", profesional1.getEyeColor());
-console.log("Raza         :", profesional1.getRace());
-console.log("Retirado?    :", profesional1.getIsRetired());
-console.log("Nacionalidad :", profesional1.getNationality());
-console.log("Nº de Oscars :", profesional1.getOscarsNumber());
-console.log("Profesión    :", profesional1.getProfession());
-
 // let profesional1: Professional = new Professional("Charlize Theron", 46, "Femenino", 58, 177, "Rubio", "Azul", "Caucásico", false, "Sudafricana", 1, "Actriz");
 // Probar método printProfessional
 let arrayProfesionales: Professional[] = [profesional1, profesional2, profesional3, profesional4, profesional5, profesional6];
